b2e025c4904331a20d42ad54def1e36bba27c453
tdias93 <thiago.olimpio93@gmail.com>
2023-05-06 15:52:41 -0300

    Ajsute_Aula_0503

--- js/main.js
+++ js/main.js
@@ -1,5 +1,38 @@
 const controle = document.querySelectorAll('[data-controle]')
-// console.log(controle)
+const estatisticas = document.querySelectorAll('[data-estatistica]')
+const pecas = {
+    "bracos": {
+        "forca": 29,
+        "poder": 35,
+        "energia": -21,
+        "velocidade": -5
+    },
+
+    "blindagem": {
+        "forca": 41,
+        "poder": 20,
+        "energia": 0,
+        "velocidade": -20
+    },
+    "nucleos":{
+        "forca": 0,
+        "poder": 7,
+        "energia": 48,
+        "velocidade": -24
+    },
+    "pernas":{
+        "forca": 27,
+        "poder": 21,
+        "energia": -32,
+        "velocidade": 42
+    },
+    "foguetes":{
+        "forca": 0,
+        "poder": 28,
+        "energia": 0,
+        "velocidade": -2
+    }
+}
 
 controle.forEach( (elemento) => {
 
@@ -7,6 +40,7 @@ controle.forEach( (elemento) => {
         // console.log(evento)
         // console.log(evento.target.dataset.controle)
         manipulaDados(evento.target.parentNode, evento.target.dataset.controle)
+        atualizaEstatistica(evento.target.dataset.peca)
     })
 
 } )
@@ -24,5 +58,15 @@ function manipulaDados(controle, operacao) {
     } else {
         alert('O valor não pode ser inferiro a 0.')
     }
+
+}
+
+
+function atualizaEstatistica(peca) {
+
+    estatisticas.forEach( (elemento) => {
+        console.log(elemento.dataset.estatistica)
+        elemento.textContent = parseInt(elemento.textContent) + pecas[peca][elemento.dataset.estatistica]
+    })
 
 }
